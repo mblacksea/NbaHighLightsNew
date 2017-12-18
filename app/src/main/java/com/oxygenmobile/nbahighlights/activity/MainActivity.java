145f38d4dcad66102a884eb53d4a3ed0fbb5c743
akem87 <mustafa.blacksea93@gmail.com>
2017-12-19 00:51:18 +0300

    First

--- app/src/main/java/com/oxygenmobile/nbahighlights/activity/MainActivity.java
+++ app/src/main/java/com/oxygenmobile/nbahighlights/activity/MainActivity.java
@@ -333,7 +333,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
 
         // show menu only when home fragment is selected
         if (navItemIndex == 0) {
